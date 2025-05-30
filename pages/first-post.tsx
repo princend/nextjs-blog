aaaf065620ab412ce331ae358dbf32688aadb2f0
Princend <waxdji3g4y3c04@gmail.com>
2025-05-30 16:42:38 +0800

    c14

--- pages/first-post.tsx
+++ pages/first-post.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import Link from "next/link";
 import Layout from "../components/layout";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function FirstPost() {
   return (<>
     <Layout>
       <Head>
         <title>First Post</title>
+
+        <Script src="https://connect.facebook.net/en_US/sdk.js"
+         strategy="beforeInteractive"
+         onLoad={() => {
+          console.log("script loaded correctly, window.FB has been populated")
+         }}
+         />
       </Head>
       <h1>First Post</h1>
       <h2>
